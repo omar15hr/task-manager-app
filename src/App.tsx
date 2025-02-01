aec8ca671f11f131c258177c61a0f8502942fb7e
omar <omarhrn@gmail.com>
2025-01-31 22:13:56 -0300

    Inicio de Nuevo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
-import { TasksList } from "./components/TasksList";
-import { Sidebar } from "./components/Sidebar";
-import { Board } from "./interfaces/types";
 
 function App() {
 
-  const [boardSelected, setBoardSelected] = useState<Board | null>(null);
-
   return (
     <div className="app">
-      <Sidebar setBoardSelected={setBoardSelected} />
-      <TasksList boardSelected={boardSelected} />
+      
     </div>
   );
 }
