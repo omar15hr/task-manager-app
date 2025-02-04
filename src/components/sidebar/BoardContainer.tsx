d7cbe7125f6e1b4c06f35281f6a42c5a0b10fe5c
omar <omarhrn@gmail.com>
2025-02-04 13:49:36 -0300

    Listo el sidebar completamente

--- src/components/sidebar/BoardContainer.tsx
+++ src/components/sidebar/BoardContainer.tsx
@@ -2,16 +2,19 @@ import { useSortable } from "@dnd-kit/sortable";
 import { BoardWithId } from "../../interfaces/types";
 import { CSS } from "@dnd-kit/utilities";
 import { PencilSvg, TrashSvg } from "../Svg";
+import { useUserActions } from "../../hooks/useBoardsActions";
 
 interface BoardProps {
   board: BoardWithId;
   handleBoardId: (id: string) => void;
   selectedBoardId: string | null;
+  openEditModal: (board: BoardWithId) => void;
 }
 
-export function BoardContainer({ board, handleBoardId, selectedBoardId  }: BoardProps) {
+export function BoardContainer({ board, handleBoardId, selectedBoardId, openEditModal  }: BoardProps) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: board.id });
+  const { removeBoard } = useUserActions();
 
   const style = { transform: CSS.Transform.toString(transform), transition };
 
@@ -21,7 +24,7 @@ export function BoardContainer({ board, handleBoardId, selectedBoardId  }: Board
       ref={setNodeRef}
       {...attributes}
       {...listeners}
-      className={`p-2 rounded-4xl shadow-md text-black my-2 w-full cursor-pointer border-2 
+      className={`p-2 rounded-4xl text-black my-2 w-full cursor-pointer border-2 
         ${selectedBoardId === board.id ? "border-[#4661cc]" : "border-[#191B1F]"} 
         hover:border-[#4661cc]`}
       onClick={() => handleBoardId(board.id)}
@@ -32,9 +35,10 @@ export function BoardContainer({ board, handleBoardId, selectedBoardId  }: Board
           <h1 className="text-white">{board.name}</h1>
         </div>
         <div className="flex flex-row gap-2 items-center justify-center text-white w-1/4">
-          <TrashSvg size={24} onClick={() => handleBoardId(board.id)} />
-          <PencilSvg size={24} onClick={() => handleBoardId(board.id)} />
+          <div onClick={() => removeBoard(board.id)}><TrashSvg size={24} /></div>
+          <div onClick={() => openEditModal(board)}><PencilSvg size={24} /></div>
         </div>
+        
       </div>
     </div>
   );
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -13,6 +13,8 @@ import {
 import { BoardWithId } from "../../interfaces/types";
 import { BoardContainer } from "./BoardContainer";
 import NewBoard from "./NewBoard";
+import { useState } from "react";
+import { NewBoardModal } from "./NewBoardModal"; // Importa el modal
 
 interface SidebarProps {
   isOpen: boolean;
@@ -31,47 +33,72 @@ export function Sidebar({
   handleBoardId,
   handleDragEnd,
   boards,
-  selectedBoardId
+  selectedBoardId,
 }: SidebarProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingBoard, setEditingBoard] = useState<BoardWithId | null>(null);
+
+  const openEditModal = (board: BoardWithId) => {
+    setEditingBoard(board);
+    setIsModalOpen(true);
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen(false);
+    setEditingBoard(null);
+  };
+
   return (
-    <nav
-      className={`fixed left-0 bottom-0 flex flex-col items-end h-full bg-[#191B1F] pt-6 pb-8 transition-all duration-300 ${
-        isOpen ? "w-80" : "w-16"
-      }`}
-    >
-      <button
-        type="button"
-        className="py-4 px-2 text-2xl text-white hover:text-gray-200"
-        onClick={toggleSidebar}
+    <>
+      <nav
+        className={`fixed left-0 bottom-0 flex flex-col items-end h-full bg-[#191B1F] pt-6 pb-8 transition-all duration-300 ${
+          isOpen ? "w-80" : "w-16"
+        }`}
       >
-        {isOpen ? <CloseSvg size={24} /> : <BaselineSvg size={24} />}
-      </button>
-      {isOpen && (
-        <div className="px-4 pb-6 self-start w-full">
-          <div className="flex flex-col w-full">
-            <DndContext
-              sensors={sensors}
-              collisionDetection={closestCenter}
-              onDragEnd={handleDragEnd}
-            >
-              <SortableContext
-                items={boards}
-                strategy={verticalListSortingStrategy}
+        <button
+          type="button"
+          className="py-4 px-2 text-2xl text-white hover:text-gray-200"
+          onClick={toggleSidebar}
+        >
+          {isOpen ? <CloseSvg size={24} /> : <BaselineSvg size={24} />}
+        </button>
+        {isOpen && (
+          <div className="px-4 pb-6 self-start w-full">
+            <div className="flex flex-col w-full">
+              <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
               >
-                {boards.map((board) => (
-                  <BoardContainer
-                    key={board.id}
-                    board={board}
-                    handleBoardId={handleBoardId}
-                    selectedBoardId={selectedBoardId}
-                  />
-                ))}
-              </SortableContext>
-            </DndContext>
-            <NewBoard />
+                <SortableContext
+                  items={boards}
+                  strategy={verticalListSortingStrategy}
+                >
+                  {boards.map((board) => (
+                    <BoardContainer
+                      key={board.id}
+                      board={board}
+                      handleBoardId={handleBoardId}
+                      selectedBoardId={selectedBoardId}
+                      openEditModal={openEditModal} 
+                    />
+                  ))}
+                </SortableContext>
+              </DndContext>
+              <NewBoard />
+            </div>
           </div>
-        </div>
+        )}
+      </nav>
+
+      {isModalOpen && editingBoard && (
+        <NewBoardModal
+          setIsVisible={setIsModalOpen}
+          toggleModal={toggleModal}
+          board={editingBoard}
+          isEditing={true}
+        />
       )}
-    </nav>
+    </>
   );
 }
