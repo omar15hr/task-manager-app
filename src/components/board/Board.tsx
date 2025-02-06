e3919b6c2ce8525ac665d159f37aaaf76e4ec400
omar <omarhrn@gmail.com>
2025-02-06 18:39:53 -0300

    Separacion de componentes

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,27 +1,93 @@
-import { useAppSelector } from "../../hooks/store";
-import { BoardWithId } from "../../interfaces/types";
-import { ColumnContainer } from "./ColumnContainer";
+import { useBoardActions } from "../../hooks/useBoardActions";
+import { BoardWithId, TaskWithId } from "../../interfaces/types";
 
 interface BoardProps {
-  isSidebarOpen: boolean;
-  boardSelected: BoardWithId;
+  selectedBoard: BoardWithId | null;
+  tasks: TaskWithId[];
 }
 
-export function Board({ isSidebarOpen, boardSelected }: BoardProps) {
-  const tasks = useAppSelector((state) =>
-    state.boards.find((board) => board.id === boardSelected.id)?.tasks || []
-  );
+export function Board({ selectedBoard, tasks }: BoardProps) {
+
+    const { addTask } = useBoardActions();
+  
+
+  const handleSubmitTask = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const title = formData.get("title") as string;
+    const status = formData.get("status") as string;
+    const background = formData.get("background") as string | null;
+    const tag = formData.get("tag") as string;
+    const columnId = selectedBoard!.id;
+
+    addTask({
+      id: crypto.randomUUID(),
+      title,
+      status,
+      background,
+      tag,
+      columnId,
+    });
+  };
 
   return (
-    <div className="flex flex-row gap-4 p-4">
-      {["Backlog", "In Progress", "In Review", "Completed"].map((status) => (
-        <ColumnContainer
-          key={status}
-          status={status}
-          tasks={tasks.filter((task) => task.status === status)}
-          isSidebarOpen={isSidebarOpen}
-        />
-      ))}
-    </div>
-  );
+    <div className="w-2/4 flex justify-center">
+        <div className="bg-gray-700"></div>
+        <div className="flex flex-col">
+          {tasks.map((task) => (
+            <div key={task.id} className="flex bg-gray-700 rounded-full p-4">
+              {task.title}
+            </div>
+          ))}
+        </div>
+        <form
+          onSubmit={handleSubmitTask}
+          className="flex flex-col bg-gray-800 p-4"
+        >
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            className="border-2 border-gray-500 rounded-full p-1 text-white"
+          />
+
+          <label htmlFor="status" className="mt-5">
+            Status
+          </label>
+          <select
+            name="status"
+            className="border-2 border-gray-500 rounded-full p-2"
+          >
+            <option value="backlog" className="bg-gray-700">
+              Backlog
+            </option>
+            <option value="completed" className="bg-gray-700">
+              Completed
+            </option>
+          </select>
+
+          <label htmlFor="tag" className="mt-5">
+            Tag
+          </label>
+          <select
+            name="tag"
+            className="border-2 border-gray-500 rounded-full p-2"
+          >
+            <option value="front-end" className="bg-gray-700">
+              Front-end
+            </option>
+            <option value="back-end" className="bg-gray-700">
+              Back-end
+            </option>
+          </select>
+
+          <button type="submit" className="p-2 rounded-full bg-gray-400 mt-4">
+            Save
+          </button>
+        </form>
+      </div>
+  )
 }
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,26 +1,65 @@
-import { useAppSelector } from "../../hooks/store";
-import { BoardContainer } from "./BoardContainer";
+import { useBoardActions } from "../../hooks/useBoardActions";
+import { BoardWithId } from "../../interfaces/types";
 
 interface SidebarProps {
-  isSidebarOpen: boolean;
-  toggleSidebar: () => void;
-  selectBoard: (boardId: string) => void;
+  boards: BoardWithId[];
+  setSelectedBoard: (board: BoardWithId) => void;
 }
 
-export function Sidebar({ isSidebarOpen, toggleSidebar, selectBoard }: SidebarProps) {
-  const boards = useAppSelector((state) => state.boards);
+export function Sidebar({ boards, setSelectedBoard }: SidebarProps) {
+
+    const { addBoard } = useBoardActions();
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const name = formData.get("name") as string;
+    const emoji = formData.get("emoji") as string;
+
+    addBoard({ name, emoji, tasks: [] });
+  };
+
 
   return (
-    <div
-      className={`fixed top-0 left-0 h-[100vh] p-4 transition-all duration-300 ease-in-out ${
-        isSidebarOpen ? "w-[250px]" : "w-[64px]"
-      }`}
-    >
-      <button onClick={toggleSidebar} className="flex items-center justify-center w-8 h-8 rounded-full">
-        {isSidebarOpen ? "Close" : "Open"}
-      </button>
+    <div className="w-2/4 flex justify-center">
+        <form onSubmit={handleSubmit} className="flex flex-col bg-gray-800 p-4">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="border-2 border-gray-500 rounded-full p-1 text-white"
+          />
 
-      {isSidebarOpen && <BoardContainer boards={boards} selectBoard={selectBoard} />}
-    </div>
-  );
+          <label htmlFor="emoji" className="mt-5">
+            Emoji
+          </label>
+          <select
+            name="emoji"
+            className="border-2 border-gray-500 rounded-full p-2"
+          >
+            <option value="🏠">🏠</option>
+            <option value="🏢">🏢</option>
+          </select>
+
+          <button type="submit" className="p-2 rounded-full bg-gray-400 mt-4">
+            Save
+          </button>
+        </form>
+
+        <div className="flex flex-row">
+          {boards.map((board) => (
+            <div
+              key={board.id}
+              className="p-4 bg-gray-600 rounded-full cursor-pointer"
+              onClick={() => setSelectedBoard(board)}
+            >
+              {board.name}
+            </div>
+          ))}
+        </div>
+      </div>
+  )
 }
